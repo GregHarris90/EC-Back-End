4ca8e9591a1265b8a89d67a3630f397cba984c7d
Greg Harris <ghart.productions@gmail.com>
2021-03-26 21:25:42 -0700

    added get one method for products

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -27,10 +27,32 @@ router.get('/', async (req, res) => {
   }
 });
 
-// get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+// GET ONE product
+router.get('/:id', async (req, res) => {
+  try {
+    // find a single product by its `id`
+    const productData = await Product.findByPk(req.params.id, {
+      // be sure to include its associated Category and Tag data
+      include: [
+        {
+          model: Category,
+          attributes: ['id', 'category_name']
+        },
+        {
+          model: Tag,
+        },
+      ]
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+    }
+
+    res.status(200).json(productData);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product

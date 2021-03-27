d0ac067100a50c213ea128a71934dbef5736be0b
Greg Harris <ghart.productions@gmail.com>
2021-03-26 22:08:19 -0700

    added POST & GET ONE route for tag

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -28,13 +28,47 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+// GET ONE tag
+router.get('/:id', async (req, res) => {
+  try {
+    // find a single tag by its `id`
+    const tagData = await Tag.findByPk(req.params.id, {
+      // be sure to include its associated Product data
+      include: [
+        {
+          model: Product,
+          attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        },
+        {
+          model: ProductTag,
+          attributes: ['id', 'product_id', 'tag_id']
+        },
+      ]
+    });
+
+    // if not selected tag send 404 message
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+    }
+
+    // send all categories (include product & product-tag)
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+// CREATE a tag
+router.post('/', async (req, res) => {
+  try {
+    // create a new tag
+    const tagData = await Tag.create(req.body);
+
+    // send created category
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
